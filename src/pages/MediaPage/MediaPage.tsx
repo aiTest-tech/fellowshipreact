553102391789d2260e59d29f436846365a897c9b
hplaptop <hplaptop@gmail.com>
2024-12-16 15:07:21 +0530

    testing added

--- src/pages/MediaPage/MediaPage.tsx
+++ src/pages/MediaPage/MediaPage.tsx
@@ -288,131 +288,6 @@ const MediaPage = () => {
                                 </div>
                             </div>
                         </div>
-                        {/* <div className="row justify-content-center gutter-24">
-                            <div className="col-lg-4 col-md-6">
-                                <div className="portfoliosub">
-                                    <a href="blog-details.html">
-                                        <img src="assets/img/portfolio1.png" alt="img" />
-                                    </a>
-                                    <div className="pgallery">
-                                        <img src="assets/img/photogallery.png" />
-                                        <h5>
-                                            <a href="#">Photo Gallery</a>
-                                        </h5>
-                                    </div>
-                                    <div className="vector1">
-                                        <img src="assets/img/Vector.png" />
-                                    </div>
-                                    <div className="vector2">
-                                        <img src="assets/img/Vector1.png" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6">
-                                <div className="portfoliosub">
-                                    <a href="blog-details.html">
-                                        <img src="assets/img/portfolio2.png" alt="img" />
-                                    </a>
-                                    <div className="pgallery">
-                                        <img src="assets/img/videogallery.png" />
-                                        <h5>
-                                            <a href="#">Video Gallery</a>
-                                        </h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6">
-                                <div className="portpartition">
-                                    <h4>
-                                        Connect with
-                                        <br />
-                                        <span>Social Media</span>
-                                    </h4>
-                                    <div className="portsocial">
-                                        <div className="d-flex flex-wrap justify-content-between w-100">
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a href="https://www.facebook.com/" target="_blank">
-                                                                <i className="fab fa-facebook-f" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a href="#" target="_blank" className="galleryyoutube">
-                                                                <i className="fab fa-youtube" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a href="#" target="_blank" className="galleryinstagram">
-                                                                <i className="fab fa-instagram" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a
-                                                                href="https://www.facebook.com/"
-                                                                target="_blank"
-                                                                className="gallerytwitter"
-                                                            >
-                                                                <i className="fab fa-twitter" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a
-                                                                href="https://www.facebook.com/"
-                                                                target="_blank"
-                                                                className="gallerylinkedin"
-                                                            >
-                                                                <i className="fab fa-linkedin-in" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-4 col-md-4 col-4">
-                                                <div className="socialgallery">
-                                                    <ul className="socialinner ps-0 d-flex justify-content-center">
-                                                        <li>
-                                                            <a
-                                                                href="https://www.facebook.com/"
-                                                                target="_blank"
-                                                                className="gallerytelegram"
-                                                            >
-                                                                <i className="fab fa-telegram" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> */}
                         <div className="row justify-content-center gutter-24 py-6 ">
                             <div className="max-w-md rounded overflow-hidden shadow-lg">
                                 <img
@@ -436,6 +311,45 @@ const MediaPage = () => {
                                 <div className="px-6 py-4">
                                     <p className="text-gray-700 text-base">
                                         {language === 'English' ? "Field visit at GST bhavan" : "GST ભવન ખાતે ક્ષેત્રની મુલાકાત"}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="max-w-md rounded overflow-hidden shadow-lg">
+                                <img
+                                    className="w-full py-2"
+                                    src="assets/img/fellows/fellow3.jpeg"
+                                    alt="Sunset in the mountains"
+                                />
+                                <div className="px-6 py-4">
+                                    <p className="text-gray-700 text-base">
+                                        {language === 'English' ? "Field visit SWAGAT" : "ક્ષેત્ર મુલાકાત SWAGAT"}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="max-w-md rounded overflow-hidden shadow-lg">
+                                <img
+                                    className="w-full py-2"
+                                    src="assets/img/fellows/fellow3.jpeg"
+                                    alt="Sunset in the mountains"
+                                />
+                                <div className="px-6 py-4">
+                                    <p className="text-gray-700 text-base">
+                                        {language === 'English' ? "Field visit SWAGAT" : "ક્ષેત્ર મુલાકાત SWAGAT"}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="max-w-md rounded overflow-hidden shadow-lg">
+                                <img
+                                    className="w-full py-2"
+                                    src="assets/img/fellows/fellow3.jpeg"
+                                    alt="Sunset in the mountains"
+                                />
+                                <div className="px-6 py-4">
+                                    <p className="text-gray-700 text-base">
+                                        {language === 'English' ? "Field visit SWAGAT" : "ક્ષેત્ર મુલાકાત SWAGAT"}
                                     </p>
                                 </div>
                             </div>
